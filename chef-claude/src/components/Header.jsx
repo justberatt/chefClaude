7a74841a29e0eb9ae30f9a9ee7c900bfaed1d1a0
Berat Sakipi <berat.sakipi@gmail.com>
2025-01-30 02:00:48 +0100

    added the Header component, which includes an image for the logo and a heading

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="src\assets\chef-claude-icon.png" className="logo" alt="chef icon" />
+            </div>
+            <h1 className="header-heading">Chef Claude</h1>
+        </header>
+    )
+}
+
+export default Header
