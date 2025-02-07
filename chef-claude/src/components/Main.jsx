d9503ca90ad5bd0977e712725203722925050e41
Berat Sakipi <berat.sakipi@gmail.com>
2025-02-07 23:49:41 +0100

    Changed it so that the get-a-recipe button and container alltogether show up on the screen only after the user has inputed four ingredients

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -28,13 +28,15 @@ const Main = () => {
                 <section>
                     <h2>Ingredients on hand:</h2>
                     <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
-                    <div className="get-recipe-container">
+                    {
+                        ingredients.length >= 4 && <div className="get-recipe-container">
                         <div>
                             <h3>Ready for a recipe?</h3>
                             <p>Generate a recipe from your list of ingredients.</p>
                         </div>
                         <button>Get a recipe</button>
                     </div>
+                    }
                 </section>
             }
         </main>
