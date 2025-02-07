1ec07206f55fd5065b838f843cf3ee26358663f2
Berat Sakipi <berat.sakipi@gmail.com>
2025-02-07 23:44:26 +0100

    rendering the list of ingredients and the 'get a recipe'-button based on the codition that there are ingredients added by the user, if not, then the title of the list and the button will not show on the screen

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -23,9 +23,20 @@ const Main = () => {
                 />
                 <button>Add ingredient</button>
             </form>
-            <ul>
-                {ingredientsList}
-            </ul>
+            {
+                ingredients.length > 0 &&
+                <section>
+                    <h2>Ingredients on hand:</h2>
+                    <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
+                    <div className="get-recipe-container">
+                        <div>
+                            <h3>Ready for a recipe?</h3>
+                            <p>Generate a recipe from your list of ingredients.</p>
+                        </div>
+                        <button>Get a recipe</button>
+                    </div>
+                </section>
+            }
         </main>
     )
 }
