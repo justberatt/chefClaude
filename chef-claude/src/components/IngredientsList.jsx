a6756aa2627b623e638ef1e8fc91e66043cda41d
Berat Sakipi <berat.sakipi@gmail.com>
2025-02-09 23:24:12 +0100

    Created an 'IngredientsList' component inside a newly created 'components' folder. Passing down the 'ingredients' array and the 'handleGetRecipeClick' button as props from the Main component, but I am creating the 'IngredientsList' here locally now, since 'IngredientsList' component has access to the 'ingredients' array.

---
+++ chef-claude/src/components/IngredientsList.jsx
@@ -0,0 +1,21 @@
+export default function IngredientsList(props) {
+    const ingredientsList = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
+            {
+                props.ingredients.length >= 4 && <div className="get-recipe-container">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button onClick={props.handleGetRecipeClick}>Get a recipe</button>
+            </div>
+            }
+        </section>
+    )
+}
