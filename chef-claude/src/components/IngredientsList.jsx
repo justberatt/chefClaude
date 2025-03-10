62580b3920ee44e6bf0e31a4a67de2fb6fb2c570
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-10 03:05:03 +0100

    passed in the ref using forwardRef

--- chef-claude/src/components/IngredientsList.jsx
+++ chef-claude/src/components/IngredientsList.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
+import { forwardRef } from 'react'
 
-export default function IngredientsList(props) {
-    const ingredientsList = props.ingredients.map(ingredient => (
+const IngredientsList = forwardRef(({ingredients, getRecipe}, ref) => {
+    const ingredientsList = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
@@ -10,19 +11,23 @@ export default function IngredientsList(props) {
             <h2>Ingredients on hand:</h2>
             <ul className="ingredients-list" aria-live="polite">{ingredientsList}</ul>
             {
-                props.ingredients.length >= 4 && <div className="get-recipe-container">
-                <div ref={props.ref}>
+                ingredients.length >= 4 && <div className="get-recipe-container">
+                <div ref={ref}>
                     <h3>Ready for a recipe?</h3>
                     <p>Generate a recipe from your list of ingredients.</p>
                 </div>
-                <button onClick={props.getRecipe}>Get a recipe</button>
+                <button onClick={getRecipe}>Get a recipe</button>
             </div>
             }
         </section>
     )
-}
+})
+
+IngredientsList.displayName = "IngredientsList"; //Added this to stop the ESLint errors
 
 IngredientsList.propTypes = {
     ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
     getRecipe: PropTypes.func.isRequired,
 }
+
+export default IngredientsList
